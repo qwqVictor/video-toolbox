a378a4ad2b66200462241a1abc49b0b22a58915f
Cat_Chang <asherversusliu@gmail.com>
2022-06-09 17:40:04 +0800

    Add some delete labels, there's still something wrong

--- videobox/src/application/controller.java
+++ videobox/src/application/controller.java
@@ -5,17 +5,28 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.Tab;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.control.cell.TextFieldListCell;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.stage.Window;
@@ -27,9 +38,42 @@ public class controller {
 	public static final ObservableList<String> data2 = FXCollections.observableArrayList();
 	public static final ObservableList<String> video3 = FXCollections.observableArrayList();
 	public static final ObservableList<String> music = FXCollections.observableArrayList();
+	Map <String,Label> map1=new HashMap<>();
+	Map <String,Label> map2=new HashMap<>();
+	Map <String,Label> map3=new HashMap<>();
 	@FXML
     private ListView<String> musiclist;
+	
+    @FXML
+    private Label label;
 
+    @FXML
+    private Label label1;
+
+    @FXML
+    private Label label2;
+
+    @FXML
+    private Label label3;
+
+    @FXML
+    private Label label4;
+
+    @FXML
+    private Label label5;
+
+    @FXML
+    private Label label6;
+
+    @FXML
+    private Label label7;
+
+    @FXML
+    private Label label8;
+
+    @FXML
+    private Label label9;
+    
     @FXML
     private Tab tab1;
 
@@ -63,6 +107,7 @@ public class controller {
     @FXML
     void OnMouseClick1(MouseEvent event) {
     	try {
+    		Label[] labellist = {label,label1,label2,label3,label4,label5,label6,label7,label8,label9};
 			Stage primaryStage = new Stage();
 			FileChooser file = new FileChooser();
 			file.setTitle("Open File");
@@ -70,6 +115,7 @@ public class controller {
 			if(list!=null) {
 				for (int i = 0; i < list.size(); i++) {
 					data.add(list.get(i).toString());
+					map1.put(list.get(i).toString(),labellist[i]);
 				}
 				videolist1.setItems(data);
 			}
@@ -83,6 +129,7 @@ public class controller {
     @FXML
     void OnMouseClick2(MouseEvent event) {
     	try {
+    		Label[] labellist = {label,label1,label2,label3,label4,label5,label6,label7,label8,label9};
 			Stage primaryStage = new Stage();
 			FileChooser file = new FileChooser();
 			file.setTitle("Open File");
@@ -90,9 +137,10 @@ public class controller {
 			if(list!=null) {
 				for (int i = 0; i < list.size(); i++) {
 					data2.add(list.get(i).toString());
+					map2.put(list.get(i).toString(),labellist[i]);
 				}
 				videolist2.setItems(data2);
-			}
+			} 	
 		}catch(Exception e){	
 			Alert alert = new Alert(Alert.AlertType.INFORMATION);
 			alert.setContentText("The video tool box is error");
@@ -103,13 +151,12 @@ public class controller {
     @FXML
     void OnMouseClick3(MouseEvent event) {
     	try {
-    		System.out.print("1");
+    		Label[] labellist = {label,label1,label2,label3,label4,label5,label6,label7,label8,label9};
 			Stage primaryStage = new Stage();
 			FileChooser file = new FileChooser();
 			file.setTitle("Open File");
 			List<File> list = file.showOpenMultipleDialog(primaryStage); 
-			if(list!=null) {
-				
+			if(list!=null) {				
 				for (int i = 0; i < list.size(); i++) {
 					String[] strs = list.get(i).toString().split("\\.");
 					if(strs[strs.length-1].equals("mp3")) {
@@ -117,13 +164,16 @@ public class controller {
 					}else {
 						video3.add(list.get(i).toString());
 					}
+					map3.put(list.get(i).toString(),labellist[i]);
 				}
 				if(video3!=null) {
 					videolist3.setItems(video3);
+
 				}
 				if(music!=null) {
 					musiclist.setItems(music);
 				}
+				
 			}
 		}catch(Exception e){	
 			Alert alert = new Alert(Alert.AlertType.INFORMATION);
@@ -145,5 +195,39 @@ public class controller {
 			alert.show();
 		}  	
     }
+    
+    @FXML
+    void OMClabel(MouseEvent event) {
+    	
+    }
+    
+    @FXML
+    void OnMouseEntered(MouseEvent event) {
+    	videolist1.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+    	videolist1.setEditable(true);
+		videolist1.setCellFactory(TextFieldListCell.forListView());
+		
+		Set<String> set=map1.keySet();
+		Iterator<String> it=set.iterator();
+		System.out.println(it.hasNext());
+		while(it.hasNext()){
+			Label lab = map1.get(it.next());
+			lab.setVisible(true);
+		}
+		
+    }
+
+
+    @FXML
+    void OnMouseExited(MouseEvent event) {
+    	Set<String> set=map1.keySet();
+		Iterator<String> it=set.iterator();
+		System.out.println(it.hasNext());
+		while(it.hasNext()){
+			Label lab = map1.get(it.next());
+			lab.setVisible(false);
+		}
+    }
+
 }
 
--- videobox/src/module-info.java
+++ videobox/src/module-info.java
@@ -2,6 +2,7 @@ module videobox {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires java.sql;
+	requires javafx.graphics;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
